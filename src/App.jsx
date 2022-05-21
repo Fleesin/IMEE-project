6963e4b5984fd8eee178ab5e73e3d65154a560df
Fleesin <jean.quintana.123@gmail.com>
2022-05-20 23:50:54 -0500

    path cambio

--- src/App.jsx
+++ src/App.jsx
@@ -17,11 +17,11 @@ function App() {
         <Routes>
             <Route path='/' element={<App/>}/>
             <Route index element={<Home />} />
-            <Route path='login' element={<Login/>}/>
-            <Route path='register' element={<Register/>}/>
-            <Route path='inscription_est' element={<Inscription_est/>}/>
-            <Route path='inscription_inst' element={<Inscription_inst/>}/>
-            <Route path='user_type' element={<User_type/>}/>
+            <Route path='https://imee-project-fleesin.vercel.app/login' element={<Login/>}/>
+            <Route path='https://imee-project-fleesin.vercel.app/register' element={<Register/>}/>
+            <Route path='https://imee-project-fleesin.vercel.app/inscription_est' element={<Inscription_est/>}/>
+            <Route path='https://imee-project-fleesin.vercel.app/inscription_inst' element={<Inscription_inst/>}/>
+            <Route path='https://imee-project-fleesin.vercel.app/user_type' element={<User_type/>}/>
         </Routes>
       </BrowserRouter> 
     </div>
