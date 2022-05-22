35be36ab0aa9bc859357aba86aebe772861fcc98
Fleesin <jean.quintana.123@gmail.com>
2022-05-21 20:55:01 -0500

    home view

--- src/components/Views/Home.jsx
+++ src/components/Views/Home.jsx
@@ -1,15 +1,24 @@
+import { Button } from '../Button';
 import React from 'react';
 import './Home.css';
+
 
 
 const Home = () =>{
     return(
         <>
-            <body class="lrbody">
-                <div>
-                    HOME
-                </div>
-            </body>
+        <section class="inicio" id="inicio">
+            <div class="content">
+                <h3>Inscripción de Matriculas <span><br/>IMEE</span></h3>
+                <p>
+                    El sistema de Inscripción de Matrículas Estudiantiles Escolares (IMEE) es una herramienta que da apoyo al proceso de matrícula estudiantil.
+                </p>
+                <a href="#"><Button>Seguir</Button></a>
+            </div>
+            <div class="image">
+                <img src="/IMEE-verde.png"/>
+            </div>
+        </section>
         </>
     )
 }
