861b9d4cf97a95d3b56f7530325e1c02fc7c78ec
Fleesin <jean.quintana.123@gmail.com>
2022-06-02 22:21:33 -0500

    profile fix

--- src/components/Views/Profiles/perfil_est.jsx
+++ src/components/Views/Profiles/perfil_est.jsx
@@ -89,8 +89,8 @@ const Perfil_est = () =>{
                     </div>
                 </div>
                 <div class="info-Nombre">
-                    <label for="text" onChange ={(e) => setApellido(e.target.value)}>Apellidos del Estudiante: {apellidos}</label>
-                    <label for="text" onChange ={(e) => setNombre(e.target.value)}>Nombres del Estudiante: {nombres}</label>
+                    <label for="text" onChange ={() => setApellido(target.value)}>Apellidos del Estudiante: {apellidos}</label>
+                    <label for="text" onChange ={() => setNombre(target.value)}>Nombres del Estudiante: {nombres}</label>
                 </div>
                 <div class="info-General">
                     <div class="input-container">
